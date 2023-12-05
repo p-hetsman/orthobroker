2fa5f8423be67029a36d7308465fb46a00c72a5d
Yuliya Simonchyk <y.simonchyk@softteco.com>
2023-12-05 18:48:32 +0300

    add:products links nav

--- src/product-templates/Products.tsx
+++ src/product-templates/Products.tsx
@@ -4,11 +4,11 @@ import Card from '@mui/material/Card';
 import CardHeader from '@mui/material/CardHeader';
 import CardContent from '@mui/material/CardContent';
 import CardActions from '@mui/material/CardActions';
-import IconButton from '@mui/material/IconButton';
-import MoreVertIcon from '@mui/icons-material/MoreVert';
 
 import { Grid } from "@mui/material";
 import data from '../data';
+import { Link } from 'react-router-dom';
+
 
 export default () => {
 
@@ -16,28 +16,30 @@ export default () => {
         <Grid container={true} gap={"40px"}>
             {data.products.map((el) => {
                 return (
-                    <Card sx={{ maxWidth: 345 }} style={{
-                        backgroundImage: `url(${el.picUrl})`, width: 240, height: 240,
-                        backgroundRepeat: 'no - repeat',
-                        backgroundSize: 'cover',
-                    }}>
-                        <CardHeader
-                            action={
-                                <IconButton aria-label="settings">
-                                    <MoreVertIcon />
-                                </IconButton>
-                            }
-                            title={el.title}
-                        />
-                        <CardContent>
+                    <div key={el.id}>
+                        <Link
+                            to={'/3d/' + el.id}
+                            style={{ textDecoration: 'none' }}
+                        >
+                            <Card sx={{ maxWidth: 345 }} style={{
+                                backgroundImage: `url(${el.picUrl})`, width: 240, height: 240,
+                                backgroundRepeat: 'no - repeat',
+                                backgroundSize: 'cover',
+                            }}>
+                                <CardHeader
+                                    title={el.title}
+                                />
+                                <CardContent>
 
-                            <CardActions>
-                            </CardActions>
-                        </CardContent>
-                    </Card>
+                                    <CardActions>
+                                    </CardActions>
+                                </CardContent>
+                            </Card>
+                        </Link>
+                    </div>
+
                 );
             })}
         </Grid>
-
     );
 };
