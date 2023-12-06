2012bce2f31acf2302510850e20d5d03e7acc5d9
Aliaksandr Bahryntsau <abahryntsau@allot.com>
2023-12-06 10:00:06 +0100

    add:company logo

--- src/Layout.tsx
+++ src/Layout.tsx
@@ -1,21 +1,23 @@
 import {
-  AppBar,
-  Layout,
-  InspectorButton,
-  TitlePortal,
-  LayoutProps,
+    AppBar,
+    Layout,
+    TitlePortal,
+    LayoutProps,
 } from "react-admin";
-import { JSX } from "react/jsx-runtime";
+import {JSX} from "react/jsx-runtime";
+import logo from './logo.png'
 
 const MyAppBar = () => (
-  <AppBar>
-    <TitlePortal />
-    <InspectorButton />
-  </AppBar>
+    <AppBar>
+        <TitlePortal/>
+        <div style={{display: 'flex', flex: 1}}>
+            <img src={logo} height={30} style={{marginRight: 10}}/>
+        </div>
+    </AppBar>
 );
 
 export default (props: JSX.IntrinsicAttributes & LayoutProps) => (
-  <>
-    <Layout {...props} appBar={MyAppBar} />
-  </>
+    <>
+        <Layout {...props} appBar={MyAppBar}/>
+    </>
 );
